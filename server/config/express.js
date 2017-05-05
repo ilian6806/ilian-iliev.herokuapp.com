df992e06d16e576223e76c9933ed97f58b16880a
ilian6806 <ilian6806@gmail.com>
2017-05-06 01:16:41 +0300

    Global favicon added

--- server/config/express.js
+++ server/config/express.js
@@ -1,7 +1,10 @@
 var express = require('express');
+var path = require('path');
 var bodyParser = require('body-parser');
+var favicon = require('serve-favicon');
 
 module.exports = function(app, config) {
+    app.use(favicon(path.join(__dirname, '../../public/images', 'favicon.ico')));
     app.use(bodyParser());
     app.use(express.static(config.rootPath + '/public'));
 };
